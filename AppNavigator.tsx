e83faca901c0140826de34a771b4a64d7c569ade
G1Azevedo <gabriel.araujo.706@ufrn.edu.br>
2025-05-27 09:59:03 -0300

    Adicionar tela de Login à navegação

--- AppNavigator.tsx
+++ AppNavigator.tsx
@@ -2,17 +2,23 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import LoginScreen from '../LoginScreen'; // Ajuste o caminho se necessário
+
 // Defina os tipos para suas rotas
 export type RootStackParamList = {
-    // Adicione as rotas conforme são implementadas
+    Login: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
     return (
-        <Stack.Navigator>
-            {/* As telas serão adicionadas aqui */}
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ headerShown: false }}
+            />
         </Stack.Navigator>
     );
 }
